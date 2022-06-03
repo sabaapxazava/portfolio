adbd6c9b69146d6e7a5d099a6ebc6aa96dba919d
sabaapxazava <60016171+sabaapxazava@users.noreply.github.com>
2022-06-03 20:16:46 +0400

    .
    .

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ $button.addEventListener('click', (e) => {
   e.preventDefault();
   $wrapper.classList.toggle('toggled');
 });
+var forEach = function (array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+		callback.call(scope, i, array[i]);
+	}
+};
+
+var forEach = function (array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+	  callback.call(scope, i, array[i]);
+	}
+};
+window.onload = function(){
+    var max = -219.99078369140625;
+    forEach(document.querySelectorAll('.pr'), function (index, value) {
+    percent = value.getAttribute('data-progress');
+      value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+      value.querySelector('.value').innerHTML = percent + '%';
+    });
+}
+
